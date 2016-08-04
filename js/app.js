a9b482177b83f42c8a5336efb3b2a2b358641b05
Joe Marzullo <jmarzu13@gmail.com>
2016-08-03 17:47:29 -0700

    api is working

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
 var app = angular.module('knowMyBlock', []);
 console.log('in the app');
+
+app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+  console.log('in the controller');
+  $http.get('https://data.seattle.gov/resource/y7pv-r3kh.json?', {
+    params: {
+      offense_type: 'ROBBERY-BUSINESS-GUN'
+    }
+  })
+  .then(function success(res) {
+    console.log(res);
+  }, function error(res) {
+    console.log(res);
+  })
+}]);
+
+
+
+// api_token = fXeV8k4r4jweDFGyUNzCQJhVw
